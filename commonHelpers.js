e5b0b89321a90ebfaa871d34ba36ea383c40a1fd
NataFilina <93826032+NataFilina@users.noreply.github.com>
2023-11-09 17:56:34 +0000

    Deploying to gh-pages from @ BendelVolodymyr/Smart-Foxes-Bookshelf@589e502e38d61c34714acc49f6d90c07fdc89446 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";import{a as t}from"./assets/vendor-26fe51b3.js";const s="https://books-backend.p.goit.global/books/";async function a(){const o="category-list";return await t.get(`${s}${o}`)}a().then(o=>console.log(o));
+//# sourceMappingURL=commonHelpers.js.map
