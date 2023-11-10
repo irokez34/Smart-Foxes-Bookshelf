e69675727aaed1a54409141da8835db7515c5669
BendelVolodymyr <134164183+BendelVolodymyr@users.noreply.github.com>
2023-11-10 23:56:09 +0000

    Deploying to gh-pages from @ BendelVolodymyr/Smart-Foxes-Bookshelf@586409a05fcd6585df670170a1caa4479d9ff289 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,18 +1,18 @@
-import"./assets/toggle_theme-e463d6d1.js";import{a as n}from"./assets/vendor-1c5e86dd.js";const c="https://books-backend.p.goit.global/books/";async function i(){const o="top-books";return await n.get(`${c}${o}`)}const g=document.querySelector(".categories-books-all");document.querySelector(".categories-books-title");window.addEventListener("load",l);async function l(){try{const o=await i(),e=b(o.data);console.log("topBooks",o),d(e)}catch(o){console.log(o)}}function b(o=[]){return o.map(({list_name:e,books:t})=>`
+import"./assets/toggle_theme-e463d6d1.js";import{a}from"./assets/vendor-1c5e86dd.js";const r="https://books-backend.p.goit.global/books/";async function c(){const o="category-list";return await a.get(`${r}${o}`)}async function l(){const o="top-books";return await a.get(`${r}${o}`)}const g={AllCategoriesContainer:document.querySelector(".all-categories-container"),AllCategoriesList:document.querySelector(".js-categories-list")};function u(o){return o.map(({list_name:e})=>`<li class="categories-list-item"><a href="#">${e}</a></li>`).join("")}c().then(o=>{g.AllCategoriesList.insertAdjacentHTML("beforeend",u(o.data))}).catch(o=>console.log(o));const b=document.querySelector(".categories-books-all");document.querySelector(".categories-books-title");window.addEventListener("load",d);async function d(){try{const o=await l(),e=k(o.data);console.log("topBooks",o),p(e)}catch(o){console.log(o)}}function k(o=[]){return o.map(({list_name:e,books:t})=>`
       <div class="categories-books-genre-title"> ${e}
         <ul class="categories-books-genre-all-books">
-        ${k(t)}
+        ${m(t)}
         </ul>
         <button class="categories-books-btn">see more</button>
       </div>
-      `).join("")}function k(o=[]){return o.map(({_id:e,list_name:t,author:a,book_image:r,title:s})=>`
+      `).join("")}function m(o=[]){return o.map(({_id:e,list_name:t,author:n,book_image:i,title:s})=>`
       <li class="categories-books-genre-book" data-id="${e}" data-genre="${t}">
         <div class="categories-books-genre-book-img-thumb">
           <img class="categories-books-genre-book-img"
-            src="${r}" alt="Title - '${s}'">
+            src="${i}" alt="Title - '${s}'">
         </div>
         <h2 class="categories-books-genre-book-title">${s}</h2>
-        <h3 class="categories-books-genre-book-author">${a}</h3>
+        <h3 class="categories-books-genre-book-author">${n}</h3>
       </li>
-      `).join("")}function d(o){g.insertAdjacentHTML("beforeend",o)}
+      `).join("")}function p(o){b.insertAdjacentHTML("beforeend",o)}
 //# sourceMappingURL=commonHelpers.js.map
