38dac09c521eff5d927edf5c27f28c63eb39ec12
NataFilina <filinanatash@gmail.com>
2023-11-10 19:09:29 +0200

    всі каткгорії + адаптив

--- src/js/BOOKS_API.js
+++ src/js/BOOKS_API.js
@@ -1,23 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 
 const BASE_URL = 'https://books-backend.p.goit.global/books/';
 
 export async function getCategoryList() {
-    const params = 'category-list';
-    return await axios.get(`${BASE_URL}${params}`);
-};
+  const params = 'category-list';
+  return await axios.get(`${BASE_URL}${params}`);
+}
 
 export async function getTopBooks() {
-    const params = 'top-books';
-    return await axios.get(`${BASE_URL}${params}`);
-};
-getCategoryList().then(data => console.log(data))
+  const params = 'top-books';
+  return await axios.get(`${BASE_URL}${params}`);
+}
+// getCategoryList().then(data => console.log(data))
 
 export async function getBooksByCategory(selectedCategory) {
-    const params = 'category?category=';
-    return await axios.get(`${BASE_URL}${params}${selectedCategory}`);
-};
+  const params = 'category?category=';
+  return await axios.get(`${BASE_URL}${params}${selectedCategory}`);
+}
 
 export async function getBooksId(idBook) {
-    return await axios.get(`${BASE_URL}${idBook}`)
-};
+  return await axios.get(`${BASE_URL}${idBook}`);
+}
--- src/js/categories_book.js
+++ src/js/categories_book.js
@@ -1,16 +1,54 @@
+import { all } from 'axios';
+import { getTopBooks, getBooksByCategory } from './BOOKS_API';
+
 const categoriesBook = document.querySelector('.categories-books-all');
 const categoriesBooksTitle = document.querySelector('.categories-books-title');
 
-function createCard(data = []) {
-  return data
-    .map(({ books: { _id, list_name, author, book_image, title } }) => {
+window.addEventListener('load', onLoad);
+
+async function onLoad() {
+  try {
+    const topBooks = await getTopBooks();
+    const categories = createCategories(topBooks.data);
+    console.log('topBooks', topBooks);
+    addCategoriesMarkup(categories);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function createCategories(arr = []) {
+  return arr
+    .map(({ list_name, books }) => {
       return `
-        <li class="categories-books-genre-book" data-id="${_id}" data-genre="${list_name}">
-            <img class="categories-books-genre-book-img" src="${book_image}" alt="Title - ${title}">
-            <h2 class="categories-books-genre-book-title">${title}</h2>
-            <h3 class="categories-books-genre-book-author">${author}</h3>
-         </li>
-        `;
+      <div class="categories-books-genre-title"> ${list_name}
+        <ul class="categories-books-genre-all-books">
+        ${createCard(books)}
+        </ul>
+        <button class="categories-books-btn">see more</button>
+      </div>
+      `;
     })
     .join('');
+}
+
+function createCard(books = []) {
+  return books
+    .map(({ _id, list_name, author, book_image, title }) => {
+      return `
+      <li class="categories-books-genre-book" data-id="${_id}" data-genre="${list_name}">
+        <div class="categories-books-genre-book-img-thumb">
+          <img class="categories-books-genre-book-img"
+            src="${book_image}" alt="Title - '${title}'">
+        </div>
+        <h2 class="categories-books-genre-book-title">${title}</h2>
+        <h3 class="categories-books-genre-book-author">${author}</h3>
+      </li>
+      `;
+    })
+    .join('');
+}
+
+function addCategoriesMarkup(markup) {
+  categoriesBook.insertAdjacentHTML('beforeend', markup);
 }
