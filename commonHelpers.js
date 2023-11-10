03f158cc986535cc530bc17cd2fca56a89aa0ae2
BendelVolodymyr <134164183+BendelVolodymyr@users.noreply.github.com>
2023-11-10 23:58:54 +0000

    Deploying to gh-pages from @ BendelVolodymyr/Smart-Foxes-Bookshelf@d9cb3e98d25e075c0f797c83e6eaba9a9515ce2d 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,18 +1,29 @@
-import"./assets/toggle_theme-e463d6d1.js";import{a}from"./assets/vendor-1c5e86dd.js";const r="https://books-backend.p.goit.global/books/";async function c(){const o="category-list";return await a.get(`${r}${o}`)}async function l(){const o="top-books";return await a.get(`${r}${o}`)}const g={AllCategoriesContainer:document.querySelector(".all-categories-container"),AllCategoriesList:document.querySelector(".js-categories-list")};function u(o){return o.map(({list_name:e})=>`<li class="categories-list-item"><a href="#">${e}</a></li>`).join("")}c().then(o=>{g.AllCategoriesList.insertAdjacentHTML("beforeend",u(o.data))}).catch(o=>console.log(o));const b=document.querySelector(".categories-books-all");document.querySelector(".categories-books-title");window.addEventListener("load",d);async function d(){try{const o=await l(),e=k(o.data);console.log("topBooks",o),p(e)}catch(o){console.log(o)}}function k(o=[]){return o.map(({list_name:e,books:t})=>`
-      <div class="categories-books-genre-title"> ${e}
+import"./assets/toggle_theme-e463d6d1.js";import{a as n}from"./assets/vendor-1c5e86dd.js";const i="https://books-backend.p.goit.global/books/";async function l(){const e="category-list";return await n.get(`${i}${e}`)}async function g(){const e="top-books";return await n.get(`${i}${e}`)}async function d(e){const o="category?category=";return await n.get(`${i}${o}${e}`)}const b={AllCategoriesContainer:document.querySelector(".all-categories-container"),AllCategoriesList:document.querySelector(".js-categories-list")};function k(e){return e.map(({list_name:o})=>`<li class="categories-list-item"><a href="#">${o}</a></li>`).join("")}l().then(e=>{b.AllCategoriesList.insertAdjacentHTML("beforeend",k(e.data))}).catch(e=>console.log(e));const c=document.querySelector(".categories-books-all"),u=document.querySelector(".categories-books-title"),m=document.querySelector(".loader");window.addEventListener("load",y);c.addEventListener("click",f);async function y(){m.style.display="none";try{const e=await g(),o=$(e.data);h(o)}catch(e){console.log(e)}}function $(e=[]){return e.map(({list_name:o,books:t})=>`
+      <div class="categories-books-genre-title"> ${o}
         <ul class="categories-books-genre-all-books">
-        ${m(t)}
+        ${p(t)}
         </ul>
         <button class="categories-books-btn">see more</button>
       </div>
-      `).join("")}function m(o=[]){return o.map(({_id:e,list_name:t,author:n,book_image:i,title:s})=>`
-      <li class="categories-books-genre-book" data-id="${e}" data-genre="${t}">
+      `).join("")}function p(e=[]){return e.map(({_id:o,list_name:t,author:s,book_image:a,title:r})=>`
+      <li class="categories-books-genre-book" data-id="${o}" data-genre="${t}">
         <div class="categories-books-genre-book-img-thumb">
           <img class="categories-books-genre-book-img"
-            src="${i}" alt="Title - '${s}'">
+            src="${a}" alt="Title - '${r}'">
         </div>
-        <h2 class="categories-books-genre-book-title">${s}</h2>
-        <h3 class="categories-books-genre-book-author">${n}</h3>
+        <h2 class="categories-books-genre-book-title">${r}</h2>
+        <h3 class="categories-books-genre-book-author">${s}</h3>
       </li>
-      `).join("")}function p(o){b.insertAdjacentHTML("beforeend",o)}
+      `).join("")}function h(e){c.insertAdjacentHTML("beforeend",e)}async function f(e){if(!e.target.classList.contains("categories-books-btn"))return;const t=e.target.previousElementSibling.firstElementChild.dataset.genre;u.innerHTML=t;try{const s=await d(t),a=C(s.data);L(a)}catch(s){console.log(s)}}function C(e=[]){return e.map(({_id:o,list_name:t,author:s,book_image:a,title:r})=>`
+      <div class="categories-books-genre-all-books">
+        <div class="categories-books-genre-books" data-id="${o}" data-genre="${t}">
+          <div class="categories-books-genre-book-img-thumb">
+            <img class="categories-books-genre-book-img"
+              src="${a}" alt="Title - '${r}'">
+          </div>
+          <h2 class="categories-books-genre-book-title">${r}</h2>
+          <h3 class="categories-books-genre-book-author">${s}</h3>
+        </div>
+      </div> 
+      `).join("")}function L(e){c.innerHTML=e}
 //# sourceMappingURL=commonHelpers.js.map
