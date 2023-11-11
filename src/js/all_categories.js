9808deb4a973a290d889c18beafa288975a4084d
AQR88 <timahaua@gmail.com>
2023-11-11 16:27:20 +0200

    AllCategories_always_first

--- src/js/all_categories.js
+++ src/js/all_categories.js
@@ -1,28 +1,41 @@
 import { getCategoryList } from '../js/BOOKS_API';
 
 const elem = {
-   AllCategoriesContainer: document.querySelector('.all-categories-container'),
-   AllCategoriesList: document.querySelector('.js-categories-list'),
-
+  AllCategoriesContainer: document.querySelector('.all-categories-container'),
+  AllCategoriesList: document.querySelector('.js-categories-list'),
 };
 
 // Функція створення розмітки
 
 function createMarkupCategoryList(arr) {
-      return arr
-        .map(({list_name})=>
-            `<li class="categories-list-item"><a href="#">${list_name}</a></li>`
-        )
-        .join('');
-    }
+  return arr
+    .map(
+      ({ list_name }) =>
+        `<li class="categories-list-item"><a href="#">${list_name}</a></li>`
+    )
+    .join('');
+}
 
-// Рендеринг розмітки
+// Рендеринг
+
+allCategoriesStatic();
 
 getCategoryList()
-.then((object) =>{
+  .then(object => {
     elem.AllCategoriesList.insertAdjacentHTML(
-    'beforeend',
-    createMarkupCategoryList(object.data)
-  )}
-)
-.catch(error => console.log(error));
+      'beforeend',
+      createMarkupCategoryList(object.data)
+    );
+  })
+  .catch(error => console.log(error));
+
+// 'All Categories' завжди перше в списку
+ 
+function allCategoriesStatic() {
+  const staticName = document.createElement('li');
+  staticName.classList.add(
+    'categories-list-item',
+  'category-active');
+  staticName.textContent = 'All Categories';
+  elem.AllCategoriesList.prepend(staticName);
+}
