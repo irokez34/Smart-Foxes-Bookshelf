6d49751124915ef36437efdc465a132c9a52c302
AQR88 <timahaua@gmail.com>
2023-11-12 14:39:46 +0200

    Update all_categories.js

--- src/js/all_categories.js
+++ src/js/all_categories.js
@@ -33,6 +33,9 @@ getCategoryList()
 
 // Функція кліку по категорії
 
+
+// СПРОБА 1
+
 // function onCategoryClick(evt) {
 //   if (!evt.target.classList.contains('.js-categories-list-item')) {
 //     return;
@@ -49,6 +52,10 @@ getCategoryList()
 //     getBooksByCategory(categoryName);
 //   }
 // }
+
+
+// СПРОБА 2
+
 // elem.allCategoriesContainer.addEventListener('click', onCategoryClick);
 
 // async function onCategoryClick(evt) {
@@ -67,42 +74,46 @@ getCategoryList()
 //     console.log(err);
 //   }
 // }
-elem.allCategoriesList.addEventListener('click', onCategoryClick);
 
-async function onCategoryClick(evt) {
 
-  evt.preventDefault();
+// СПРОБА 3
+
+// elem.allCategoriesList.addEventListener('click', onCategoryClick);
+
+// async function onCategoryClick(evt) {
+
+//   evt.preventDefault();
   
-  if (!evt.target.classList.contains('js-categories-list-item')) {
-    return;
-  }
-  const arrClass = [...elem.allCategoriesList.children];
-  arrClass.map(item => item.classList.remove('category-active'));
-  evt.target.classList.add('category-active');
+//   if (!evt.target.classList.contains('js-categories-list-item')) {
+//     return;
+//   }
+//   const arrClass = [...elem.allCategoriesList.children];
+//   arrClass.map(item => item.classList.remove('category-active'));
+//   evt.target.classList.add('category-active');
 
-  const categoryLink = evt.target.dataset.category;
+//   const categoryLink = evt.target.dataset.category;
 
-  const genreByWord = categoryLink.split(' ');
-  const lastWord = genreByWord.splice(-1, 1);
+//   const genreByWord = categoryLink.split(' ');
+//   const lastWord = genreByWord.splice(-1, 1);
   
-  elem.categoriesBooksTitle.innerHTML = `${genreByWord.join(
-    ' '
-    )} <span class="categories-books-title-accent">${lastWord.toString()}</span>`;
+//   elem.categoriesBooksTitle.innerHTML = `${genreByWord.join(
+//     ' '
+//     )} <span class="categories-books-title-accent">${lastWord.toString()}</span>`;
 
 
 
-  // categoriesBook.innerHTML = `${genreByWord.join(
-  //   ' '
-  // )} <span class="categories-books-title-accent">${lastWord.toString()}</span>`;
-  elem.categoriesBooksTitle.insertAdjacentHTML('afterend');
+//   // categoriesBook.innerHTML = `${genreByWord.join(
+//   //   ' '
+//   // )} <span class="categories-books-title-accent">${lastWord.toString()}</span>`;
+//   elem.categoriesBooksTitle.insertAdjacentHTML('afterend');
 
-  try {
-    const booksByListName = await getBooksByCategory(categoryLink);
+//   try {
+//     const booksByListName = await getBooksByCategory(categoryLink);
 
-    const allBooksByGenre = createCardByGenre(booksByListName.data);
+//     const allBooksByGenre = createCardByGenre(booksByListName.data);
 
-    addCardByGenre(allBooksByGenre);
-  } catch (err) {
-    console.log(err);
-  }
-}
+//     addCardByGenre(allBooksByGenre);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
