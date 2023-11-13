6a058cf18e40fefcf77c051afaa5911b93c37ff8
Yunna <73770647+anita224@users.noreply.github.com>
2023-11-13 18:00:23 +0100

    support-arrow

--- src/js/suport.js
+++ src/js/suport.js
@@ -1,16 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('additionalItems').style.display = 'none';
+});
+
 function toggleExpand() {
-  var additionalItems = document.getElementById('additionalItems');
   var expandBtn = document.querySelector('.expand-btn');
+  var additionalItems = document.getElementById('additionalItems');
 
-  additionalItems.style.display =
-    additionalItems.style.display === 'none' ||
-    additionalItems.style.display === ''
-      ? 'flex'
-      : 'none';
-
-  // Добавление/удаление класса "expanded" для изменения стилей стрелки
   expandBtn.classList.toggle(
     'expanded',
-    additionalItems.style.display === 'flex'
+    additionalItems.style.display == 'none'
   );
+
+  additionalItems.style.display =
+    additionalItems.style.display == 'none' ? 'flex' : 'none';
 }
